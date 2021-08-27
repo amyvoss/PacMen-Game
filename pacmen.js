561107947c82e26add02487abc95cea83767e59c
amyvoss <85448994+amyvoss@users.noreply.github.com>
2021-08-26 23:18:56 -0400

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -4,9 +4,8 @@ const pacArray = [
   ['./PacMan3.png', './PacMan4.png'],
 ];
 let direction = 0;
-const pacMen = []; // This array holds all the pacmen
+const pacMen = []; 
 
-// This function returns an object with random values
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
@@ -14,26 +13,25 @@ function setToRandom(scale) {
   };
 }
 
-// Factory to make a PacMan at a random position with random velocity
 function makePac() {
-  // returns an object with random values scaled {x: 33, y: 21}
+  
   let velocity = setToRandom(10); // {x:?, y:?}
   let position = setToRandom(200);
 
-  // Add image to div id = game
+  
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
   newimg.src = './PacMan1.png';
   newimg.width = 100;
 
-  // TODO: set position here
+  
   newimg.style.left = position.x;
   newimg.style.top = position.y;
-  // TODO add new Child image to game
+  
   game.appendChild(newimg);
 
-  // return details in an object
+  
   return {
     position,
     velocity,
@@ -42,7 +40,7 @@ function makePac() {
 }
 
 function update() {
-  // loop over pacmen array and move each one and move image in DOM
+  
   pacMen.forEach((item) => {
     checkCollisions(item);
     item.position.x += item.velocity.x;
@@ -55,7 +53,7 @@ function update() {
 }
 
 function checkCollisions(item) {
-  // TODO: detect collision with all walls and make pacman bounce
+  
     if (
     item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
     item.position.x + item.velocity.x < 0
@@ -70,10 +68,10 @@ function checkCollisions(item) {
 }
 
 function makeOne() {
-  pacMen.push(makePac()); // add a new PacMan
+  pacMen.push(makePac()); 
 }
 
-//don't change this line
+
 if (typeof module !== 'undefined') {
   module.exports = { checkCollisions, update, pacMen };
 }
